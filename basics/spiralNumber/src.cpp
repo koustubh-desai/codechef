52d03b6cf5dd39d1ef0cc6c116d8c8999fe4a720
koustubh-desai <koustubh-desai@users.noreply.github.com>
2019-05-25 00:06:35 +0530

    Add files via upload
    vector operations example in spiralNumber.

--- basics/spiralNumber/src.cpp
+++ basics/spiralNumber/src.cpp
@@ -16,19 +16,75 @@
     std::cout.rdbuf(out.rdbuf()); \
     std::string line; 
 using namespace std;
+std::vector<vector <int>> v;
+vector<vector<int>> top(vector<vector<int>> v){
+    if(v.size() == 0) return v;
+     vector<int> i = v.front();
+     for(vector<int>::iterator it=i.begin();it<i.end();it++){ 
+        cout<<*it<<" ";
+     }
+     v.erase(v.begin());
+     return v;
+}
+vector<vector<int>> right(vector<vector<int>> v){
+    if(v.size() == 0) return v;
+     rep(i,0,v.size()){
+        //int n = v[i].pop_back();
+        cout<<v[i][v[i].size()-1]<<" ";
+        v[i].pop_back();
+     }
+     return v;
+}
+vector<vector<int>> bottom(vector<vector<int>> v){
+    if(v.size() == 0) return v;
+     vector<int> i = v.back();
+     for(vector<int>::reverse_iterator it=i.rbegin();it!=i.rend();it++){
+        cout<<*it<<" ";
+     }
+     v.pop_back();
+     return v;
+}
+vector<vector<int>> left(vector<vector<int>> v){
+    
+    if(v.size()==0) return v;
+    //for(vector<vector<int>>::iterator it=v.begin();it!=v.end();it++){
+    rep(i,0,v.size()){
+        vector<int> c = v[i];
+        cout<<c[0]<<" ";
+        c.erase(c.begin());
+        v[i] = c;
+    }
+    return v;
+}
 int main(){
     changeInput;
 	int i,j;
+    
     cin>>i;
-    cout<<"I got "<<i;
     while(i--){
-    	cin>>j;
-    	cout<<"\n got "<<j;
-    	if (cin.get() == '\n'){
-    		cout<<"\n I got an enter key";
-    	}
+        vector <int> a;
+        while(1){
+            cin>>j;
+            a.push_back(j);
+            if (cin.get() == '\n') break;
+        }
+        v.push_back(a);
+        a.clear();
+
+    }
+    cout<<"\n vector is ...";
+    while(v.size()){
+        v = top(v);
+        v = right(v);
+        v = bottom(v);
+        v = left(v);
     }
     
+        // rep(i,0,v.size()){
+        //     rep(j,0,v[i].size()){
+        //         cout<<"\n a["<<i<<"]["<<j<<"]="<<v[i][j];
+        //     }
+        // }
     
     /*cout<<" got "<<i;
     getline(cin,line);
