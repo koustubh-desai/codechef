81513d79b35cd1c5b50057a07200ad1bafacd6e7
koustubh-desai <koustubh-desai@users.noreply.github.com>
2020-05-31 23:18:10 +0530

    Successful Submit for BSTOPS

--- codechef/practice/beginner/BSTOPS/src.cpp
+++ codechef/practice/beginner/BSTOPS/src.cpp
@@ -10,106 +10,121 @@
 using namespace std;
 
 struct Node{
-	int value;
-	int place;
-	Node *left = NULL;
-	Node *right = NULL;
+	int value,position;
+	Node *left = NULL, *right = NULL;
+	Node(int val, int pos){
+		this->value = val;
+		this->position = pos;
+	}
 };
-Node *head = NULL;
-Node* findRightMost(Node *a, int *valueOfTarget, char *found){
+
+/*void traverseForFun(Node *a,int place){
+	if(a == NULL){
+		cout<<" what the heck !!! Found none at "<<place<<endl;
+		return;
+	}
+	if(a->left != NULL) traverseForFun(a->left,place<<1);
+	cout<<" Fun "<<a->value<<" at "<<place<<endl;
+	if(a->right != NULL) traverseForFun(a->right,(place<<1)+1);
+}*/
+Node* findRightMost(Node *a, int *valueOfTarget, int *found, int place){
 	if( a != NULL){
 		//In else if it has no children. Then a is our dude. b =a
 		if(a->right != NULL){
-			a->right= findRightMost(a->right, valueOfTarget, found);
+			a->right= findRightMost(a->right, valueOfTarget, found, (place<<1)+1);
 			return a;
 		}
-		else if(a->right == NULL && a->left == NULL){
-			cout<<" Found "<<a->value<<" in findRightMost at position "<<a->place<<endl;
-			*found = 'y';
+		else{
+			//a->right is NULL
+			*found = place;
 			*valueOfTarget = a->value;
+			if(a->left != NULL) {
+				Node *tbr = a->left;
+				delete a;
+				return tbr;
+			};
 			return NULL;
 		}
 	}
 	return a;
 }
-Node* findLeftMost(Node *a, int *valueOfTarget, char *found){
+Node* findLeftMost(Node *a, int *valueOfTarget, int *found, int place){
 	if( a != NULL){
 		//In else if See if it has no children. Then a is our dude. b =a
 		if(a->left != NULL){
-			a->right= findLeftMost(a->left, valueOfTarget, found);
+			a->left= findLeftMost(a->left, valueOfTarget, found, place<<1);
 			return a;
 		}
-		else if(a->right == NULL && a->left == NULL){
-			cout<<" Found "<<a->value<<" in findLeftMost at position "<<a->place<<endl;
-			*found = 'y';
+		else{
+			//a->left is NULL
+			*found = place;
 			*valueOfTarget = a->value;
+			if(a->right != NULL) {
+				Node *tbr = a->right;
+				delete a;
+				return tbr;
+			}
 			return NULL;
 		}
 	}
 	return a;
 }
-Node* traverseToDelete(Node *node,int val){
-	
+Node* traverseToDelete(Node *node,int place,int val){
 	if(node==NULL) return node;
-	
-	if(node->value ==  val){
-		
+	if(val > node->value) node->right = traverseToDelete(node->right, (place<<1)+1, val);
+	else if(val < node->value) node->left = traverseToDelete(node->left,place<<1, val);
+	else if(node->value ==  val){
+		cout<<node->position<<endl;
 		//1. no children
 		if(node->left == NULL && node->right == NULL){
-			delete node;
+			free(node);
 			return NULL;
 		}
 		//2. got two children
 		if(node->left != NULL && node->right != NULL){
 			int valueOfTarget;
-			char found = 'n';
-			node->left = findRightMost(node->left, &valueOfTarget, &found);
-			if(found != 'n'){
-				cout<<" Found in left treee child with value"<<valueOfTarget<<" at position"<<endl;
-			}
-			else{
-				node->right = findLeftMost(node->right, &valueOfTarget, &found);
-				cout<<" Found in right tree child with value"<<valueOfTarget<<" at position "<<endl;
-			}
+			int found = 0;
+			node->right = findLeftMost(node->right, &valueOfTarget, &found,(place<<1)+1);
+			//if(!found) node->left = findRightMost(node->left, &valueOfTarget, &found,place<<1);
+			node->value = valueOfTarget;
 			return node;
 		}
 		//3. got one child
 		else{
 			Node *toBeReturned= (node->left != NULL)?node->left:node->right;
-			delete node;
+			free(node);
 			return toBeReturned;
 		}
 	}
-	if(val > node->value) node->right = traverseToDelete(node->right, val);
-	else node->left = traverseToDelete(node->left, val);
 	return node;
+}
+Node* delete_node(Node *root,int val, bool flag){
+	
 }
 Node* traverseToInsert(Node *node, int place, int val){
 	if(node == NULL){
-		node = new Node();
-		node->value = val;
-		node->place = place;
+		node = new Node(val,place);
 		cout<<place<<endl;
-		return node;
 	}
-	if(val > node->value) node->right = traverseToInsert(node->right, node->place*2+1, val); 
-	if(val < node->value) node->left = traverseToInsert(node->left, node->place*2, val);
+	else if(val > node->value) node->right = traverseToInsert(node->right,(place<<1)+1, val); 
+	else if(val < node->value) node->left = traverseToInsert(node->left, place<<1, val);
 	return node;
 }
 int main(){
 	ios_base::sync_with_stdio(false); 
 	cin.tie(NULL);
-	std::ifstream in("test.txt");
+	/*std::ifstream in("test.txt");
 	std::streambuf*cinbuf = std::cin.rdbuf(); // save old buff
-	std::cin.rdbuf(in.rdbuf()); //redirect std::cin to be in  .txt!
+	std::cin.rdbuf(in.rdbuf()); //redirect std::cin to be in  .txt!*/
 	std::string line;
+	Node *head = NULL;
 	int N,T,temp;
 	char Instr;
 	cin>>T;
 	while(T--){
 		cin>>Instr;
 		cin>>N;
-		head = (Instr == 'i')?traverseToInsert(head,1,N):traverseToDelete(head,N);
+		head = (Instr == 'i')?traverseToInsert(head,1,N):traverseToDelete(head,1,N);
 	}
 	return 0;
 }
