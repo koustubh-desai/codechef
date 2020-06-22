0b3896556a5e3cfe5e2005e9826a8e15fe2445e7
koustubh-desai <koustubh-desai@users.noreply.github.com>
2020-06-22 09:57:05 +0530

    Now working H1

--- codechef/practice/beginner/H1/src.cpp
+++ codechef/practice/beginner/H1/src.cpp
@@ -5,49 +5,149 @@
 #include <fstream>
 #include <iomanip>
 #include <tuple>
+#include <bits/stdc++.h>
 #define rep(i,a,b)  for(int i=a;i<b;i++)
+#define rep_inverse(i,a,b)  for(int i=a;i>=b;i--)
 #define max(a,b) a>b?a:b
-#define take(arr) for(int i=0; i<3; i++) for(int j=0; j<3; j++) cin>>arr[i][j];
-#define out(arr) for(int i=0; i<3; i++) {for(int j=0; j<3; j++) cout<<arr[i][j]<<" "; cout<<endl;}
+#define input(v) for(int i=0; i <9; i++) cin>>v[i]
+#define output(v) for(int i=0; i <9; i++) cout<<v[i]<<" "
+#define isPrimeNumber(s) (s==3 || s==5 || s==7 || s==11 || s==13 || s==17) 
 
-#define in2(arr) cin>>arr[0][0]>>arr[0][1]>>arr[0][2]>>arr[1][0]>>arr[1][1]>>arr[1][2]>>arr[2][0]>>arr[2][1]>>arr[2][2];
-#define out2(arr) cout<<arr[0][0]<<arr[0][1]<<arr[0][2]<<"\n"<<arr[1][0]<<arr[1][1]<<arr[1][2]<<"\n"<<arr[2][0]<<arr[2][1]<<arr[2][2]<<endl;
-#define twod vector<vector <int> >
-//is array sorted
-#define issolved(a) (a[0][0] == 1) && (a[0][1] == 2) && (a[0][2] == 3) && (a[1][0] == 4) && (a[1][1] == 5) && (a[1][2] == 6) && (a[2][0] == 7) && (a[2][1] == 8) && (a[2][2] == 9);
-#define isvalid(i,j,k,l) (i>=0) && (i<=2) && (j>=0) && (j<=2) && (k>=0) && (k<=2) &&(l>=0) && (l<=2) && ((i==k-1 && j==l) || (i==k+1 && j==l) || (i==k && j==l-1) || (i==k-1 && j==l+1))
-#define isprime(n) n==2 || n==3 || n==5 || n==7 || n==11 || n==13 || n==17
-#define isend(i,j) (i==2 && j==2)
-using namespace std;
-int ssort(twod arr, int i, int j, int m, int n, int count, bool firstTime){
-	if issolved(arr) return count;
-	if(firstTime){
-		cout<<"FIRSTOS"<<issolved(arr);
-		vector <int> counts;
-		firstTime = false;
-		for(i = 0; i<=2 i++) for(j = 0; j<=2; j++) {
-			counts.push_back(ssort(arr,i,j,i-1,j,0,false));
-			counts.push_back(ssort(arr,i,j,i+1,j,0,false));
-			counts.push_back(ssort(arr,i,j,i,j-1,0,false));
-			counts.push_back(ssort(arr,i,j,i,j+1,0,false));
-		}
-		
-		i=0; j=0; m = 0; n = 1;
-	}
-	else{
-		if(isvalid(i,j,m,n) && isprime(arr[i][j] + arr[m][n])){
-			//swap
-			int temp = arr[i][j];
-			arr[i][j] = arr[m][n];
-			arr[m][n] = temp;
-			//increment count
-			++count;
-			//check ifsolved
-			//check ifend
+using namespace std; 
+vector<int> sorted = {1,2,3,4,5,6,7,8,9};
+//vector<int> sorted = {7,3,2,4,1,5,6,8,9};
+int calls = 0;
+int limit = 100;
+int getPos(char pos, int index){
+	
+	switch(pos){
+		case 'l':
+			switch(index){
+				case 0: return -1;
+				case 1: return 0;
+				case 2: return 1;
+				case 3: return -1;
+				case 4: return 3;
+				case 5: return 4;
+				case 6: return -1;
+				case 7: return 6;
+				case 8: return 7;
+				default: return -2;
+			}
+			break;
+		case 't':
+			switch(index){
+				case 0: return -1;
+				case 1: return -1;
+				case 2: return -1;
+				case 3: return 0;
+				case 4: return 1;
+				case 5: return 2;
+				case 6: return 3;
+				case 7: return 4;
+				case 8: return 5;
+				default: return -2;
+			}
+			break;
+		case 'b':
+			switch(index){
+				case 0: return 3;
+				case 1: return 4;
+				case 2: return 5;
+				case 3: return 6;
+				case 4: return 7;
+				case 5: return 8;
+				case 6: return -1;
+				case 7: return -1;
+				case 8: return -1;
+				default: return -2;
+			}
+			break;
+		case 'r':
+			switch(index){
+				case 0: return 1;
+				case 1: return 2;
+				case 2: return -1;
+				case 3: return 4;
+				case 4: return 5;
+				case 5: return -1;
+				case 6: return 7;
+				case 7: return 8;
+				case 8: return -1;
+				default: return -2;
+			}
+			break;
+		default: return -2;
 			
-		}
 	}
-	return count;
+	
+}
+bool isValid(int currentFrom, int currentTo, int prevTo, int prevFrom, vector<int> &v){
+	if(currentTo <0 || currentFrom < 0) return false;
+	if((currentFrom == prevFrom && currentTo==prevTo) || (currentFrom == prevTo && currentTo==prevFrom)) return false;
+	if(!isPrimeNumber(v[currentFrom]+v[currentTo])) return false;
+	if(v[currentFrom] == currentFrom+1) return false;
+	if(v[currentTo] == currentTo+1) return false;
+	return true;
+}
+void ssort(vector<int> v, int from, int to, int count, vector<int> &arr,int indexx,bool found){
+	if(++calls>=limit) return;
+	if(indexx%9){
+		//cout<<" One cycle is over and found is "<<found<<endl;
+		if(!found) {
+			cout<<" GANJAAAAA returning"<<endl;
+			return;
+		}
+		found = false;
+	}
+	int temp;
+	int current = indexx%9;
+	//swap values
+	if(from >=0 && to >= 0 && from!=to){
+		cout<<" For arr"<<endl;
+		output(v);
+		cout<<" swap pos from="<<from<<" swap pos to="<<to<<" val1="<<v[from]<<" val2="<<v[to]<<endl;
+		temp = v[from];
+		v[from] = v[to];
+		v[to] = temp;
+	}
+	if(v == sorted) {
+		cout<<"WOOOHOOOOO"<<count<<endl;
+		cout<<" and size of arr is "<<arr.size()<<endl;
+		arr.push_back(count);
+		sort(arr.begin(), arr.end(),[](int a,int b){return a<b;}); 
+		return;
+	}
+	rep(i,current,8){
+	//rep_inverse(i,8,current){
+		 //check left
+		 temp = getPos('l',i);
+		 if(isValid(i,temp,to,from,v)){
+		 	found = true;
+		 	ssort(v,i,temp,count+1,arr,i,found);
+		 }
+		 
+		  //check right
+		 temp = getPos('r',i);
+		 if(isValid(i,temp,to,from,v)) {
+		 	found = true;
+		 	ssort(v,i,temp,count+1,arr,i,found);
+		 }
+		 
+		  //check top
+		 temp = getPos('t',i);
+		 if(isValid(i,temp,to,from,v)) {
+		 	found = true;
+		 	ssort(v,i,temp,count+1,arr,i,found);
+		 }
+		 
+		  //check bottom
+		 temp = getPos('b',i);
+		 if(isValid(i,temp,to,from,v)) {
+		 	found = true;
+		 	ssort(v,i,temp,count+1,arr,i,found);
+		 }
+	}
 }
 int main(){
 	ios_base::sync_with_stdio(false); 
@@ -56,14 +156,15 @@ int main(){
 	std::streambuf*cinbuf = std::cin.rdbuf(); // save old buff
 	std::cin.rdbuf(in.rdbuf()); //redirect std::cin to be in  .txt!
 	std::string line;
-	int N,T,temp;
-	char Instr;
+	int N,T;
+	
 	cin>>T;
 	while(T--){
-		twod arr =  {{0,0,0},{0,0,0},{0,0,0}};
-		take(arr);
-		//cout<<ssort(arr,-1,-1,-1,-1,-1,true)<<endl;
-		ssort(arr,-1,-1,-1,-1,-1,true);
+		vector<int> count = {};
+		vector<int> v(9);
+		input(v);
+		cout<<"GETTING STARTED"<<endl;
+		ssort(v,-1,-1,0,count,0,false);
 	}
 	return 0;
 }
